e2c6b61eda740d902f05c442bbd6279d2fbfc893
dugurca <dugurca@gmail.com>
2016-01-23 11:55:48 +0200

    init

---
+++ Assets/_A/Scripts/MockPoint.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MockPoint
+{
+	public float scale = 0.1f;
+	public Vector3 scaleVec;
+
+	public MockPoint() 
+	{
+		scaleVec = new Vector3(scale, scale, scale);
+	}
+
+	public MockPoint(float s, Vector3 sv)
+	{
+		scale = s;
+		scaleVec = sv;
+	}
+
+	public GameObject CreateSphere()
+	{
+		var go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		go.transform.localScale = scaleVec;
+		go.GetComponent<Collider>().enabled = false;
+		return go;
+	}
+
+	public GameObject CreateSphereAtPos(Vector3 pos)
+	{
+		var go = CreateSphere();
+		go.transform.position = pos;
+		return go;
+	}
+}
---
+++ Assets/_A/Scripts/SendRays.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SendRays : MonoBehaviour
+{
+	private const float RaySpacing = 0.005f;
+	private const float Sc = 0.25f;
+	readonly Vector3 _scaleVec = new Vector3(Sc, Sc, Sc);
+
+	private Camera _rayCam;
+	void Awake()
+	{
+		_rayCam = GetComponent<Camera>();
+	}
+
+	void Rays()
+	{
+		MockPoint mp = new MockPoint(Sc, _scaleVec);
+		Ray ray;
+		RaycastHit hit;
+		for (float i = 0f; i <= 1f; i += RaySpacing)
+		{
+			for (float j = 0f; j <= 1; j += RaySpacing)
+			{
+				ray = _rayCam.ViewportPointToRay(new Vector3(i, j, 0));
+				if (Physics.Raycast(ray, out hit))
+				{
+					mp.CreateSphereAtPos(hit.point);
+				}
+			}
+		}
+	}
+	
+	void Start ()
+	{
+		Rays();
+	}
+}
